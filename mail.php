8d6aa89a2e1fa3447a64cece7ae02be964b6cbae
Егор Михайлов <fsociety@MacBook-Pro-Egor.local>
2020-08-07 04:20:16 +0700

    UpdMailer

--- mail.php
+++ mail.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $name = $_POST['name'];
 $phone = $_POST['tell'];
@@ -6,9 +6,10 @@ $email = $_POST['mail'];
 $age = $_POST['age'];
 $answerFirst = $_POST['prof'];
 $answerSecond = $_POST['cash'];
-$message = 'Имя: ' .$name . '<br>' . 'Возраст: ' .$age . '<br>' . 'Телефон: ' . $phone . '<br>' . 'Почта: ' . $email . '<br>' . 
-'Ответ на первый вопрос: ' . $answerFirst . '<br>' . 'Ответ на второй вопрос: ' . $answerSecond;
+$message = 'Имя: ' .$name . "\n" . 'Возраст: ' .$age . "\n" . 'Телефон: ' . $phone . "\n" . 'Почта: ' . $email . "\n" .
+'Имеется ли опыт сдачи недвижимости в долгосрочную аренду?: ' . $answerFirst . "\n" . 'Есть ли возможность совмещения с основной деятельностью?: ' . $answerSecond;
 
 mail('noare.association@gmail.com', 'Оставлена новая заявка на сайте', $message);
 
 ?>
+
